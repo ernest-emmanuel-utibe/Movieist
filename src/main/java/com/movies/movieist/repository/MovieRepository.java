202dca977a72b54ed5b12dba7dccbe2733018c9f
Ernest Emmanuel Utibe <em123nest@gmail.com>
2024-03-06 17:36:14 +0100

    Update MovieRepository.java

--- src/main/java/com/movies/movieist/repository/MovieRepository.java
+++ src/main/java/com/movies/movieist/repository/MovieRepository.java
@@ -4,7 +4,6 @@ import com.movies.movieist.model.Movies;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
